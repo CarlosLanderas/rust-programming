0bdc71f0f51b5ac90c4238d7bbcb2effd47f3d72
Carlos Landeras <carlos.landeras@gmail.com>
2019-07-30 18:30:20 +0200

    parse_complex tests

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -14,6 +14,13 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     }
 }
 
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
+    }
+}
+
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
@@ -33,4 +40,17 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
     assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+
+    assert_eq!(parse_complex(",-0.0625"), None);
+    assert_eq!(parse_complex("notnumber, 0.23"), None);
 }
