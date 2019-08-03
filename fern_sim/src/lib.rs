0e061521ed4692be2f247ed5af98e91f45e4e80f
Carlos Landeras <carlos.landeras@gmail.com>
2019-08-03 17:00:43 +0200

    src/bin fern sim entry point

--- fern_sim/src/lib.rs
+++ fern_sim/src/lib.rs
@@ -8,11 +8,11 @@ impl Fern {
     pub fn grow(&mut self) {
         self.size *= 1.0 + self.growth_rate;
     }
+}
 
-    ///Run a fern simulation for some number of days
-    pub fn run_simulation(fern: &mut Fern, days: usize) {
-        for _ in 0..days {
-            fern.grow();
-        }
+///Run a fern simulation for some number of days
+pub fn run_simulation(fern: &mut Fern, days: usize) {
+    for _ in 0..days {
+        fern.grow();
     }
 }
