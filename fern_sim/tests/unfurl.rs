89d3cddeee3828af8b12baf17f056c23e287d7f1
Carlos Landeras <carlos.landeras@gmail.com>
2019-08-03 18:23:42 +0200

    Add test files

--- fern_sim/tests/unfurl.rs
+++ fern_sim/tests/unfurl.rs
@@ -3,9 +3,9 @@ use std::time::Duration;
 
 #[test]
 fn test_fiddlehead_unfurling() {
-    // let mut world = Terrarium::Load("tests/unfurl_files/fiddlehead.tm");
-    // assert!(world.fern(0),is_furled());
-    // let one_hour = Duration::from_secs(60 * 60);
-    // world.apply_sunlight(one_hour);
-    // assert!(world.fern(0).is_fully_unfurled());
+    let mut world = Terrarium::load("tests/unfurl_files/fiddlehead.tm");
+    assert!(world.fern(0).is_furled());
+    let one_hour = Duration::from_secs(60 * 60);
+    world.apply_sunlight(one_hour);
+    assert!(world.fern(0).is_fully_unfurled());
 }
