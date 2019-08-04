b822aa62592ac03f2c74b7a5fbb3f2b19aea26fd
Carlos Landeras <carlos.landeras@gmail.com>
2019-08-04 16:05:43 +0200

    Add interval ordering tests

--- snippets/src/generic_operators.rs
+++ snippets/src/generic_operators.rs
@@ -1,53 +1,89 @@
+use std::cmp::{Ordering, PartialEq, PartialOrd};
 use std::ops::{Add, Neg};
-use std::cmp::{PartialEq};
 ///Struct representing a Complex number
 #[derive(Debug)]
 pub struct Complex<T> {
     pub re: T,
-    pub im: T
+    pub im: T,
 }
 
 impl<T> PartialEq for Complex<T>
-    where T: PartialEq {
-        fn eq(&self, other: &Complex<T> ) -> bool {
-            self.re == other.re && self.im == other.im
-        }
+where
+    T: PartialEq,
+{
+    fn eq(&self, other: &Complex<T>) -> bool {
+        self.re == other.re && self.im == other.im
     }
+}
 
 impl<T> Add for Complex<T>
-    where T: Add<Output=T> {
-        type Output = Self;
-        fn add(self, rhs: Self) -> Self {
-            Complex {
-                re: self.re + rhs.re,
-                im: self.im + rhs.im
-            }
+where
+    T: Add<Output = T>,
+{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Complex {
+            re: self.re + rhs.re,
+            im: self.im + rhs.im,
         }
     }
+}
 
-impl<T,O> Neg for Complex<T>
-    where T: Neg<Output=O> {
-        type Output = Complex<O>;
-        fn neg(self) -> Complex<O> {
-            Complex{ re: -self.re, im: -self.im}
+impl<T, O> Neg for Complex<T>
+where
+    T: Neg<Output = O>,
+{
+    type Output = Complex<O>;
+    fn neg(self) -> Complex<O> {
+        Complex {
+            re: -self.re,
+            im: -self.im,
         }
     }
+}
+
+#[derive(Debug, PartialEq)]
+struct Interval<T> {
+    lower: T,
+    upper: T,
+}
+
+impl<T> PartialOrd<Interval<T>> for Interval<T>
+where
+    T: PartialOrd,
+{
+    fn partial_cmp(&self, other: &Interval<T>) -> Option<Ordering> {
+        match self {
+            s if s == other => Some(Ordering::Equal),
+            s if s.lower >= other.upper => Some(Ordering::Greater),
+            s if s.upper <= other.lower => Some(Ordering::Less),
+            _ => None,
+        }
+    }
+}
+
+// fn partial_cmp(&self, other: &Rhs) -> Option<Ordering>;
 
 #[test]
 fn complex_add_test() {
-    let complex1 = Complex{re: 2, im: 5};
-    let complex2 = Complex{ re: 5, im: 3};
+    let complex1 = Complex { re: 2, im: 5 };
+    let complex2 = Complex { re: 5, im: 3 };
 
-    assert_eq!(complex1 + complex2, Complex{ re: 7, im: 8});
+    assert_eq!(complex1 + complex2, Complex { re: 7, im: 8 });
 }
 
 #[test]
 fn complex_negate_test() {
-
-    let complex1 = Complex{ re: 5, im: 10};
+    let complex1 = Complex { re: 5, im: 10 };
     let complex2 = &(-complex1);
 
-   assert_eq!(complex2.re, -5);
-   assert_eq!(complex2.im, -10);
+    assert_eq!(complex2.re, -5);
+    assert_eq!(complex2.im, -10);
+}
 
+#[test]
+fn interval_ordering_test() {
+    assert!(Interval{ lower: 10, upper:20} < Interval{lower: 20, upper:40});
+    assert!(Interval{ lower: 7, upper: 8} >= Interval{lower: 0, upper: 1});
+    assert!(Interval{ lower: 7, upper: 8} <= Interval{lower: 7, upper: 8});
 }
--- snippets/src/operators.rs
+++ snippets/src/operators.rs
@@ -1,11 +1,11 @@
-use std::ops::Add;
-use std::cmp::{PartialEq,PartialOrd, Ord, Ordering, Eq};
+use std::cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd};
 use std::fmt;
+use std::ops::Add;
 
 #[derive(Debug)]
 pub struct Money {
     pub symbol: String,
-    pub value: f64
+    pub value: f64,
 }
 
 impl Add for Money {
@@ -14,96 +14,129 @@ impl Add for Money {
     fn add(self, other: Self) -> Self {
         Self {
             symbol: self.symbol,
-            value: self.value + other.value
+            value: self.value + other.value,
         }
     }
 }
 
 impl PartialOrd for Money {
-
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
-
 }
 
 impl PartialEq for Money {
-     fn eq(&self, other: &Self) -> bool {
-         self.value == other.value
-     }
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
+    }
 }
-
 
 impl Ord for Money {
     fn cmp(&self, other: &Self) -> Ordering {
-           match self.value {
+        match self.value {
             v if v == other.value => Ordering::Equal,
             v if v >= other.value => Ordering::Greater,
             v if v <= other.value => Ordering::Less,
-            _ => panic!("Invalid value")
+            _ => panic!("Invalid value"),
         }
     }
 }
 
-impl Eq for Money{}
+impl Eq for Money {}
 
 #[derive(PartialEq)]
 pub struct User {
     pub name: String,
-    pub age: usize
+    pub age: usize,
 }
-
 
 impl Add for User {
     type Output = Self;
-      fn add(self, other: Self) -> Self {
-          Self {
-              name : self.name.add(" ").add(&other.name),
-              age: self.age + other.age
-          }
-      }
+    fn add(self, other: Self) -> Self {
+        Self {
+            name: self.name.add(" ").add(&other.name),
+            age: self.age + other.age,
+        }
+    }
 }
 
 impl fmt::Debug for User {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-      write!(f, "Name: {}, Age: {}", self.name, self.age)
+        write!(f, "Name: {}, Age: {}", self.name, self.age)
     }
 }
 
 #[test]
 fn money_add_test() {
+    let money1 = Money {
+        symbol: "€".to_string(),
+        value: 10.5,
+    };
+    let money2 = Money {
+        symbol: "€".to_string(),
+        value: 9.0,
+    };
 
-    let money1 = Money{symbol : "€".to_string(), value : 10.5};
-    let money2 = Money{symbol: "€".to_string(), value: 9.0};
+    assert_eq!(
+        money1 + money2,
+        Money {
+            symbol: "€".to_string(),
+            value: 19.5
+        }
+    );
 
-    assert_eq!(money1 + money2, Money{ symbol: "€".to_string(), value: 19.5});
-
-    let money3 = Money{symbol : "€".to_string(), value : 20.5};
-    let money4 = Money{symbol: "€".to_string(), value: 10.0};
+    let money3 = Money {
+        symbol: "€".to_string(),
+        value: 20.5,
+    };
+    let money4 = Money {
+        symbol: "€".to_string(),
+        value: 10.0,
+    };
     let mut moneyvec = vec![money3, money4];
     moneyvec.sort();
 
-    assert_eq!(moneyvec,
-            vec![Money{symbol: "€".to_string(), value: 10.0},
-            Money{symbol : "€".to_string(), value : 20.5}]);
+    assert_eq!(
+        moneyvec,
+        vec![
+            Money {
+                symbol: "€".to_string(),
+                value: 10.0
+            },
+            Money {
+                symbol: "€".to_string(),
+                value: 20.5
+            }
+        ]
+    );
 }
 
 #[test]
 fn user_add_test() {
-    let user1 = User { name: "Carlos".to_string(), age: 34};
-    let user2 = User { name: "Pedro".to_string(), age: 20};
+    let user1 = User {
+        name: "Carlos".to_string(),
+        age: 34,
+    };
+    let user2 = User {
+        name: "Pedro".to_string(),
+        age: 20,
+    };
 
     let user_sum = user1 + user2;
     assert_eq!(user_sum.name, "Carlos Pedro".to_string());
     assert_eq!(user_sum.age, 54);
-
 }
 
 #[test]
 fn user_eq_test() {
-
-    let user3 =  User { name: "Pedro".to_string(), age: 20};
-    let user4 =  User { name: "Pedro".to_string(), age: 20};
+    let user3 = User {
+        name: "Pedro".to_string(),
+        age: 20,
+    };
+    let user4 = User {
+        name: "Pedro".to_string(),
+        age: 20,
+    };
 
     //This works because User implements Debug trait
     assert_eq!(user3, user4);
