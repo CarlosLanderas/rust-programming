199cdff9b3d7df8bfab3298299ec637760b9eb99
Carlos Landeras <carlos.landeras@gmail.com>
2019-08-04 14:17:06 +0200

    Add operators tests

--- snippets/src/operators.rs
+++ snippets/src/operators.rs
@@ -1,5 +1,5 @@
 use std::ops::Add;
-use std::cmp::PartialEq;
+use std::cmp::{PartialEq,PartialOrd, Ord, Ordering, Eq};
 
 #[derive(Debug)]
 pub struct Money {
@@ -18,11 +18,32 @@ impl Add for Money {
     }
 }
 
+impl PartialOrd for Money {
+
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+
+}
+
 impl PartialEq for Money {
      fn eq(&self, other: &Self) -> bool {
          self.value == other.value
      }
 }
+
+
+impl Ord for Money {
+    fn cmp(&self, other: &Self) -> Ordering {
+           match self.value {
+            v if v == other.value => Ordering::Equal,
+            v if v >= other.value => Ordering::Greater,
+            v if v <= other.value => Ordering::Less,
+            _ => panic!("Invalid value")
+        }
+    }
+}
+
 
 #[test]
 fn money_add_test() {
@@ -31,4 +52,13 @@ fn money_add_test() {
     let money2 = Money{symbol: "€".to_string(), value: 9.0};
 
     assert_eq!(money1 + money2, Money{ symbol: "€".to_string(), value: 19.5});
+
+    let money3 = Money{symbol : "€".to_string(), value : 20.5};
+    let money4 = Money{symbol: "€".to_string(), value: 10.0};
+    let mut moneyvec = vec![money3, money4];
+    moneyvec.sort();
+
+    assert_eq!(moneyvec,
+            vec![Money{symbol: "€".to_string(), value: 10.0},
+            Money{symbol : "€".to_string(), value : 20.5}]);
 }
