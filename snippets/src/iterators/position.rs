4bb38eaec789a78634ce89840f5fde8e73113dca
Carlos Landeras <carlos.landeras@gmail.com>
2019-08-08 11:16:35 +0200

    add rposition consumer sample

--- snippets/src/iterators/position.rs
+++ snippets/src/iterators/position.rs
@@ -11,5 +11,13 @@ fn position_consumer_test() {
 
     assert_eq!(m_position, Some(5));
     assert_eq!(z_position, None);
+}
+
+#[test]
+fn r_position_consumer_test() {
+    let text = "Are you a boy scout?";
+
+    assert_eq!(text.as_bytes().iter().rposition(|&c| c== b'b'), Some(10));
+    assert_eq!(text.as_bytes().iter().rposition(|&c| c== b'A'), Some(0));
 
 }
