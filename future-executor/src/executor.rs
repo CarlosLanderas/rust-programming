342d0d3112823adb7a97a4de70f3123257c64c7e
Carlos Landeras <clanderas@plainconcepts.com>
2019-08-18 08:33:43 +0200

    Implement main fn with async experimental feature

--- future-executor/src/executor.rs
+++ future-executor/src/executor.rs
@@ -23,12 +23,12 @@ impl ArcWake for Task {
     }
 }
 
-struct Executor {
+pub struct Executor {
     ready_queue: Receiver<Arc<Task>>,
 }
 
 impl Executor {
-    fn run(&self) {
+    pub fn run(&self) {
         while let Ok(task) = self.ready_queue.recv() {
             let mut future_slot = task.future.lock().unwrap();
             if let Some(mut future) = future_slot.take() {
@@ -44,12 +44,12 @@ impl Executor {
 }
 
 #[derive(Clone)]
-struct Spawner {
+pub struct Spawner {
     task_sender: SyncSender<Arc<Task>>,
 }
 
 impl Spawner {
-    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+    pub fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
         let future = future.boxed();
         let task = Arc::new(Task {
             future: Mutex::new(Some(future)),
@@ -60,7 +60,7 @@ impl Spawner {
     }
 }
 
-fn new_executor_and_spawner() -> (Executor, Spawner) {
+pub fn new_executor_and_spawner() -> (Executor, Spawner) {
     const MAX_QUEUED_TASKS : usize = 10_000;
     let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
     (Executor { ready_queue}, Spawner { task_sender})
